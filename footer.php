46cadea5f9514e54e500a453edf37df12c5debe4
Sathish Janjirala <js.kumar24@gmail.com>
2025-01-06 19:52:59 +0530

    adding php files

--- footer.php
+++ footer.php
@@ -25,15 +25,10 @@
   <title><?php echo 'ServerNEXgen'; ?></title>
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <!-- <style>
-    /* Custom CSS for the progress bar */
-    .progress {
-      top: 0;
-      left: 0;
-      right: 0;
-      margin-top: 6rem;
-    }
-  </style> -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style></style>
+</head>
 </head>
 
 <body>
@@ -53,40 +48,50 @@
         <div class="col-6-f col-md-2 mb-3 px-xl-6">
           <h5 class="footer-h5">Company</h5>
           <ul class="nav flex-column">
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">About</a></li>
-            <li class=" mb-2"><a href="#" class="nav-link-footer hover-underline-animation">Contact</a></li>
+            <li class="mb-2"><a href="aboutus.php" class="nav-link-footer hover-underline-animation">About</a></li>
+            <li class=" mb-2"><a href="contact.php" class="nav-link-footer hover-underline-animation">Contact</a></li>
           </ul>
         </div>
         <div class="col-6-f col-md-2 mb-3 px-xl-6">
           <h5 class="footer-h5">Hosting</h5>
           <ul class="nav flex-column">
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">Dedicated</a></li>
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">Shared Hostng</a></li>
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">VPS-Windows</a></li>
-            <li class=" mb-2"><a href="#" class="nav-link-footer hover-underline-animation">VPS-Linux</a></li>
+            <li class="mb-2"><a href="dedicated.php" class="nav-link-footer hover-underline-animation">Dedicated</a></li>
+            <li class="mb-2"><a href="sharedhosting.php" class="nav-link-footer hover-underline-animation">Shared Hostng</a></li>
+            <li class="mb-2"><a href="vps_win.php" class="nav-link-footer hover-underline-animation">VPS-Windows</a></li>
+            <li class=" mb-2"><a href="vps_linux.php" class="nav-link-footer hover-underline-animation">VPS-Linux</a></li>
           </ul>
         </div>
         <div class="col-6-f col-md-2 mb-3 px-xl-6">
           <h5 class="footer-h5">Services</h5>
           <ul class="nav flex-column">
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">Trading Server</a></li>
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">Tally on cloud</a></li>
-            <li class="mb-2"><a href="#" class="nav-link-footer hover-underline-animation">SAP-Services</a></li>
+            <li class="mb-2"><a href="trading.php" class="nav-link-footer hover-underline-animation">Trading Server</a></li>
+            <li class="mb-2"><a href="tally.php" class="nav-link-footer hover-underline-animation">Tally on cloud</a></li>
+            <li class="mb-2"><a href="sap.php" class="nav-link-footer hover-underline-animation">SAP-Services</a></li>
           </ul>
         </div>
       </div>
       <div class="d-flex flex-column flex-sm-row justify-content-between py-0 my-2 border-top">
         <p>&copy; 2025 ServerNEXgen, Inc. All rights reserved.</p>
         <ul class="list-unstyled d-flex">
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><img class="hover-underline-animation" src="images/twitter.png" style="background-color: #fff;" width="24" height="24">
-                <use xlink:href="#twitter" />
-              </img></a></li>
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><img src="images/instagram.png" style="background-color: #fff;" width="24" height="24">
-                <use xlink:href="#instagram" />
-              </img></a></li>
-          <li class="ms-3"><a class="link-body-emphasis" href="#"><img  src="images/facebook.png" style="background-color: #fff;" width="24" height="24">
-                <use xlink:href="#facebook" />
-              </img></a></li>
+          <li class="ms-3"><a href="#" class="fa fa-twitter" src="images/icons8-twitter-squared-30.png"></a>
+            </img>
+            </a>
+          </li>
+          <li class="ms-3"><a href="#" class="fa fa-instagram"></a>
+            <use xlink:href="#instagram" />
+            </img>
+            </a>
+          </li>
+          <li class="ms-3"><a href="#" class="fa fa-facebook"></a>
+            <use xlink:href="#facebook" />
+            </img>
+            </a>
+          </li>
+          <li class="ms-3"><a href="#" class="fa fa-linkedin"></a>
+            <use xlink:href="#linkedin" />
+            </img>
+            </a>
+          </li>
         </ul>
       </div>
 
--- index.php
+++ index.php
@@ -49,57 +49,7 @@
   </div>
 
   <header class="site-navbar" role="banner">
-
-    <!-- <div class="container">
-      <div class="row align-items-center">
-
-        <div class="col-11 col-xl-2">
-          <h1 class="mb-0 site-logo"><a href="index.php" class="text-white mb-0">ServerNEXgen</a></h1>
-        </div>
-        <div class="col-12 col-md-10 d-none d-xl-block">
-          <nav class="site-navigation position-relative text-right" role="navigation">
-
-            <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block">
-              <li class="active"><a href="index.php"><span>Home</span></a></li>
-              <li><a href="aboutus.php"><span>About us</span></a></li>
-              <li class="has-children">
-                <a href="index.php"><span>Hosting</span></a>
-                <ul class="dropdown arrow-top">
-                  <li><a href="dedicated.php">Dedicated</a></li>
-                  <li><a href="sharedhosting.php">Shared Hosting</a></li>
-                  <li><a href="#">Trading Server</a></li>
-                  <li><a href="#">Tally on cloud</a></li>
-                  <li class="has-children">
-                    <a href="#">VPS</a>
-                    <ul class="dropdown">
-                      <li><a href="#">Windows</a></li>
-                      <li><a href="#">Linux</a></li> 
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class="has-children">
-                <a href="about.php"><span>Solutions</span></a>
-                <ul class="dropdown arrow-top">
-                  <li><a href="#">Trading Server</a></li>
-                  <li><a href="#">Tally on cloud</a></li>
-                </ul>
-              </li>
-              <li><a href="contact.php"><span>SAP Services</span></a></li>
-              <li><a href="contact.php"><span>Contact</span></a></li>
-            </ul>
-          </nav>
-        </div>
-
-
-        <div class="d-inline-block d-xl-none ml-md-0 mr-auto py-3" style="position: relative; top: 3px;"><a href="#"
-            class="site-menu-toggle js-menu-toggle text-white"><span class="icon-menu h3"></span></a></div>
-
-      </div>
-    </div> -->
   </header>
-
-  <!-- <div class="hero" style="background-image: url('images/hero_1.jpg');"></div> -->
 
   <div id="myCarousel" class="carousel slide" data-ride="carousel" data-bs-pause="true" data-interval="5000">
 
