51bef264f6f53bcd1da99dda46d09d1e78aba034
Sathish Janjirala <js.kumar24@gmail.com>
2025-01-30 11:44:33 +0530

    test

--- index.php
+++ index.php
@@ -98,13 +98,13 @@ $current_page = basename($_SERVER['PHP_SELF']);
           interval = 38,//it takes about 6s, interval=20 takes about 4s
           $bar = $('.transition-timer-carousel-progress-bar'),
           $crsl = $('#myCarousel');
-        // $('.carousel-indicators li, .carousel-control').click(function () { $bar.css({ width: 0.5 + '%' }); });
+        $('.carousel-indicators li, .carousel-control').click(function () { $bar.css({ width: 0.5 + '%' }); });
         /*line above just for showing when controls are clicked the bar goes to 0.5% to make more friendly, 
         if you want when clicked set bar empty, change on width:0.5 to width:0*/
-        // $crsl.carousel({//initialize
-        //   interval: false,
-        //   pause: true
-        // }).on('slide.bs.carousel', function () { percent = 0; });//This event fires immediately when the bootstrap slide instance method is invoked.
+        $crsl.carousel({//initialize
+          interval: false,
+          pause: true
+        }).on('slide.bs.carousel', function () { percent = 0; });//This event fires immediately when the bootstrap slide instance method is invoked.
         function progressBarCarousel() {
           $bar.css({ width: percent + '%' });
           percent = percent + 0.5;
